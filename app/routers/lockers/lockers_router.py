9aa5b787a82f2fb0f734b9ee1c7c999b4717de0c
mansooranis <mansooranis60@gmail.com>
2024-09-13 17:43:51 -0400

    Added new display all lockers feature

--- app/routers/lockers/lockers_router.py
+++ app/routers/lockers/lockers_router.py
@@ -3,6 +3,7 @@ from app.utils.auth import get_firebase_user_from_token
 from app.models.Locker.methods import get_all_available_lockers, rent_locker
 from app.models.Order import Order
 from app.models.Order.methods import create_order
+from app.routers import check_admin_access
 from app import main
 # import uuid
 import uuid
@@ -52,3 +53,31 @@ async def get_my_locker(user: dict = Depends(get_firebase_user_from_token)):
     """
     locker = await main.app.mongodb["lockers"].find({"ownerEmail": user["email"]}, {"_id": 0}).to_list(None)
     return {"lockers": locker}
+
+# @router.post("/deallocate-locker")
+# async def deallocate_locker(request: Request, user: dict = Depends(get_firebase_user_from_token)):
+#     """
+#     Deallocate a locker
+#     """
+#     body = await request.json()
+#     locker_id = body.get("locker_id")
+#     if locker_id is None:
+#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Missing parameters")
+#     locker = await main.app.mongodb["lockers"].find_one({"lockerNumber": locker_id})
+#     if locker is None:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Locker not found")
+#     if locker["ownerEmail"] != user["email"]:
+#         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="You do not own this locker")
+#     return await deallocate_locker(locker_id)
+
+@router.get("/get-all-lockers")
+async def get_all_lockers(user: dict = Depends(get_firebase_user_from_token)):
+    """
+    Get all lockers
+    """
+    error = await check_admin_access(user)
+    if error:
+        raise error
+    lockers  = await main.app.mongodb["lockers"].find({}, {"_id": 0}).to_list(length=None)
+
+    return {"lockers": lockers}
