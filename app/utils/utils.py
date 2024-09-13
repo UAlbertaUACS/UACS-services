1d5d19d95da106a2f720b7de8abb03dca80a506c
mansooranis <mansooranis60@gmail.com>
2024-09-13 12:39:54 -0400

    Fixed naming.

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -2,10 +2,12 @@ def create_approved_locker_email(name:str, locker_number:int, combination:str):
     """
     Create an email for an approved locker
     """
-    return f"""Hello {name},\n\nYour locker rental has been approved. Your locker number is {locker_number} and your combination is {combination}.\n\nThank you,\nUACS"""
+    return f"""Hello,\n\nYour locker rental has been approved. Your locker number is {locker_number} and your combination is {combination}.\n\n
+    You can also view your locker details at https://lockers.uacs.ca/my-lockers. \n\nIf you have any further questions email us at
+    Thank you,\nUACS"""
 
 def create_rejected_locker_email(name:str, notes:str):
     """
     Create an email for a rejected locker
     """
-    return f"""Hello {name},\n\nYour locker rental has been rejected. Reason: {notes}.\n\nIf you have any further questions email us at execs@uacs.ca.\n\nThank you,\nUACS"""
+    return f"""Hello,\n\nYour locker rental has been rejected. Reason: {notes}.\n\nIf you have any further questions email us at execs@uacs.ca.\n\nThank you,\nUACS"""
